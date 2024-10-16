014377a14f7953782b4b310a32df22aacef34b9e
alamfauzan <gfauzan963@gmail.com>
2024-10-16 23:26:45 +0700

    hapus

--- 05_Antarmuka_Pengguna_lanjutan/flexible_expanded/README.md
+++
@@ -1,3 +0,0 @@
-# flexible_expanded
-
-A new Flutter project.
--- 05_Antarmuka_Pengguna_lanjutan/flexible_expanded/android/app/src/main/kotlin/com/example/flexible_expanded/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flexible_expanded
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
